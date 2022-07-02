caeea59ecea9c65d5afd3e9955b8ad02b4663a4f
mschof17 <mschof@mit.edu>
2022-07-02 16:02:15 -0700

    Added ROS

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mschof/BWSI_Student_Code/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/mschof/BWSI_Student_Code/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mschof/BWSI_Student_Code/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/mschof/BWSI_Student_Code/catkin_ws/src:$ROS_PACKAGE_PATH"
